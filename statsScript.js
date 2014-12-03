729e1914402ba218aafa5c9672a10f0412543c69
Jordan Komnick <jordan@komnick.com>
2014-12-02 23:26:26 -0500

    Chart changes
    There is some weird data here, so a lot of the checkboxes don't do
    anything.  There is a bug where numbers of responses can go below zero
    which I'm too tired to fix right now.  Don't refresh the page with boxes
    unchecked.  Random Number checkboxes don't change because I don't have
    data for that.

--- statsScript.js
+++ statsScript.js
@@ -12,14 +12,18 @@ var pollingInitialData = [
   ];
   var quizInitialData = [
     ['Response', '# Responses'],
-    ['Correct',  4],
-    ['Incorrect',  3]
+    ['Correct',  1],
+    ['Incorrect',  9]
   ];
   var randomNumberInitialData = [
     ['Response', '# Responses'],
     ['Correct',  4],
     ['Incorrect',  3]
   ];
+  
+  var pollingCurrentData = pollingInitialData;
+  var quizCurrentData = quizInitialData;
+  var randomNumberCurrentData = randomNumberInitialData;
   
 //once google's packages have loaded, call the drawChart function w/ initial data
 google.setOnLoadCallback(function(){ pollingDrawChart(pollingInitialData) });
@@ -36,7 +40,8 @@ var formattedData = google.visualization.arrayToDataTable(data);
 //set chart options
   var options = {
     title: 'How do you like to receive information?',
-    hAxis: {title: 'Option'}
+    hAxis: {title: 'Option'},
+	colors: ['#EF7026', 'gray']
   };
 
   //create a new chart in the div with id='chart'
@@ -56,7 +61,8 @@ var formattedData = google.visualization.arrayToDataTable(data);
 //set chart options
   var options = {
     title: 'What is the lowest price appliance to get no interest if paid in full in a year?',
-    hAxis: {title: 'Response'}
+    hAxis: {title: 'Response'},
+	colors: ['#EF7026', 'gray']
   };
 
   //create a new chart in the div with id='chart'
@@ -76,7 +82,8 @@ var formattedData = google.visualization.arrayToDataTable(data);
 //set chart options
   var options = {
     title: 'Product ID 8923 has been recalled. Notify callers.',
-    hAxis: {title: 'Response'}
+    hAxis: {title: 'Response'},
+	colors: ['#EF7026', 'gray']
   };
 
   //create a new chart in the div with id='chart'
@@ -90,27 +97,178 @@ var formattedData = google.visualization.arrayToDataTable(data);
 
 $(document).ready(function(){
 	//using jquery to check if a checkbox is checked or not whenever it is clicked
-  $("#pollingOption1").click(function(){
-    if($("#pollingOption1").is(":checked")) {
+  $("#pollingSeniorMgr2").click(function(){
+    if($("#pollingSeniorMgr2").is(":checked")) {
 	//make some new data
-	var pollingOption1CheckedData = [
-    ['Option', '# Responses'],
-    ['In Person',  4],
-    ['Email',  10],
-    ['Voice',  5],
-    ['Visual',  1]
-  ];
+	//this is weird because everyone who responded to this poll is under Kate
+	pollingCurrentData[1][1] += 4;
+	pollingCurrentData[2][1] += 3;
+	pollingCurrentData[3][1] += 3;
   //redraw the chart with the new data
-  pollingDrawChart(pollingOption1CheckedData);
+  pollingDrawChart(pollingCurrentData);
 	} else {
-		var pollingOption1UncheckedData = [
-    ['Option', '# Responses'],
-    ['In Person',  4],
-    ['Email',  3],
-    ['Voice',  3],
-    ['Visual',  0]
-  ];
-  pollingDrawChart(pollingOption1UncheckedData);
+	pollingCurrentData[1][1] -= 4;
+	pollingCurrentData[2][1] -= 3;
+	pollingCurrentData[3][1] -= 3;
+	pollingDrawChart(pollingCurrentData);
 	}
   });
+  
+  $("#pollingManager4").click(function(){
+    if($("#pollingManager4").is(":checked")) {
+	//make some new data
+	//this is weird because everyone who responded to this poll is under Matt
+	pollingCurrentData[1][1] += 4;
+	pollingCurrentData[2][1] += 3;
+	pollingCurrentData[3][1] += 3;
+  //redraw the chart with the new data
+  pollingDrawChart(pollingCurrentData);
+	} else {
+	pollingCurrentData[1][1] -= 4;
+	pollingCurrentData[2][1] -= 3;
+	pollingCurrentData[3][1] -= 3;
+	pollingDrawChart(pollingCurrentData);
+	}
+  });
+  
+  $("#quizSeniorMgr2").click(function(){
+    if($("#quizSeniorMgr2").is(":checked")) {
+	//make some new data
+	//this is weird because everyone who responded to this poll is under Kate
+	quizCurrentData[1][1] += 1;
+	quizCurrentData[2][1] += 9;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[1][1] -= 1;
+	quizCurrentData[2][1] -= 9;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizManager4").click(function(){
+    if($("#quizManager4").is(":checked")) {
+	//make some new data
+	//this is weird because everyone who responded to this poll is under Matt
+	quizCurrentData[1][1] += 1;
+	quizCurrentData[2][1] += 9;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[1][1] -= 1;
+	quizCurrentData[2][1] -= 9;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift1").click(function(){
+    if($("#quizShift1").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift2").click(function(){
+    if($("#quizShift2").is(":checked")) {
+	//make some new data
+	quizCurrentData[1][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[1][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift3").click(function(){
+    if($("#quizShift3").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift4").click(function(){
+    if($("#quizShift4").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift5").click(function(){
+    if($("#quizShift5").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 2;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 2;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift6").click(function(){
+    if($("#quizShift6").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 2;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 2;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift7").click(function(){
+    if($("#quizShift7").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizShift8").click(function(){
+    if($("#quizShift8").is(":checked")) {
+	//make some new data
+	quizCurrentData[2][1] += 1;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[2][1] -= 1;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
+  $("#quizTraining1").click(function(){
+    if($("#quizTraining").is(":checked")) {
+	//make some new data
+	quizCurrentData[1][1] += 1;
+	quizCurrentData[2][1] += 9;
+  //redraw the chart with the new data
+  quizDrawChart(quizCurrentData);
+	} else {
+	quizCurrentData[1][1] -= 1;
+	quizCurrentData[2][1] -= 9;
+  quizDrawChart(quizCurrentData);
+	}
+  });
+  
 });
